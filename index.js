0363e638821b8274852737712b745ba2f27e5398
dbsGen <z.rz2323721@gmail.com>
2018-10-06 11:59:52 +0800

    v1.0.1 添加cache

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 
 const Queue = require('./libs/queue');
+const fs = require('fs');
 
-function TaskQueue() {
+const MINUTE = 60 * 1000;
+const HOURS = 60 * MINUTE;
+const DAY = 24 * HOURS;
+
+function TaskQueue(ops) {
     this.queues = {};
+    this.options = ops;
 }
 
 TaskQueue.prototype.task = function (key, action, complete) {
@@ -21,4 +27,29 @@ TaskQueue.prototype.task = function (key, action, complete) {
     }
 };
 
+TaskQueue.prototype.cache = function (key, time, action, complete) {
+    let path = this.options.path+'/c_'+key+'.cache';
+    let stat = fs.statSync(path);
+    if (new Date().getTime() - stat.mtime.getTime() < time) {
+        complete(true, fs.readFileSync(path));
+    } else {
+        this.task(key, action, function (suc, result) {
+            if (suc) {
+                if (result instanceof String) {
+                    fs.writeFileSync(path, result);
+                } else {
+                    fs.writeFileSync(path, JSON.stringify(result));
+                }
+                complete(suc, result);
+            } else {
+                complete(suc, result);
+            }
+        });
+    }
+};
+
 module.exports = new TaskQueue();
+
+module.exports.MINUTE = MINUTE;
+module.exports.HOURS = HOURS;
+module.exports.DAY = DAY;
